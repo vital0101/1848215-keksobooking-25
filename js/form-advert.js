0fe5974fdd95e015d281b510de3de7d9550076a7
Vitaliy Vaskin <vital0101@yandex.ru>
2022-03-27 21:01:57 +0300

    добавит валидация формы

--- js/form-advert.js
+++ js/form-advert.js
@@ -1,42 +1,65 @@
 const formAdvertValidation = document.querySelector('.ad-form');
 
-const pristine = new Pristine(formAdvertValidation, {
-  classTo: 'ad-form__element',
-  errorClass: 'form__item--invalid',
-  successClass: 'form__item--valid',
-  errorTextParent: 'form__item',
-  errorTextTag: 'span',
-  errorTextClass: 'form__error'
-},
+const pristine = new Pristine(
+  formAdvertValidation,
+  {
+    classTo: 'ad-form__element',
+    errorTextParent: 'ad-form__element',
+    errorTextClass: 'ad-form__element--error-text',
+  }
 );
 
-// выбор поля валидации
 const elementTitleAdvert = formAdvertValidation.querySelector('#title');
-
-// условие валидации
-function validateTitleAdvert (value) {
-  return value.length >= 30 && value.length <= 100;
-}
+const validateTitleAdvert = (value) => value.length < 30 && value.length > 100;
+const getErrorTextTitle = () => 'От 30 до 100 символов';
 
 pristine.addValidator(
   elementTitleAdvert,
   validateTitleAdvert,
-  'От 30 до 100 символов',
+  getErrorTextTitle,
 );
 
 // выбор поля валидации
+const elementTypeRoom = formAdvertValidation.querySelector('#type');
 const elementPriceRoom = formAdvertValidation.querySelector('#price');
-
-// условие валидации
-function validatePriceAdvert (value) {
-  return value.max > 100000;
-}
-
+const minPriceHousing = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+// условие валидации поля price
+const validatePriceAdvert = () => elementPriceRoom.value > minPriceHousing[elementTypeRoom.value];
+elementTypeRoom.addEventListener('change', () => {
+  elementPriceRoom.placeholder = minPriceHousing[elementTypeRoom.value];
+  elementPriceRoom.min = minPriceHousing[elementTypeRoom.value];
+});
+const getTextErrorPrice = () => `Минимальная цена ${minPriceHousing[elementTypeRoom.value]}`;
 pristine.addValidator(
   elementPriceRoom,
   validatePriceAdvert,
-  'Максимальная сумма 100000',
+  getTextErrorPrice,
 );
+
+
+// просто хрень честно говоря, попытка написать хоть что то
+// const validateTimeIn = formAdvertValidation.querySelector('#timein');
+// const valueTimeIn = validateTimeIn.querySelectorAll('option');
+// console.log(validateTimeIn[1]);
+// const validateTimeOut = formAdvertValidation.querySelector('#timeout');
+// console.log(valueTimeIn);
+// const valueTimeOut = validateTimeOut.querySelectorAll('option');
+// console.log(validateTimeIn[1]);
+
+// validateTimeIn.addEventListener('change', () => {
+//   for (let i = 0; i < validateTimeIn.length; i++) {
+//     if(valueTimeIn[i] === valueTimeOut[i]) {
+//       valueTimeOut.setAttribute('selected', true);
+//       return;
+//     }
+//   }
+// });
 
 formAdvertValidation.addEventListener('submit', (evt) => {
   evt.preventDefault();
