f950da915aeaeb990c3b8faf1dde015a266a7776
Vitaliy Vaskin <vital0101@yandex.ru>
2022-03-29 12:03:02 +0300

    добавит правка валидации формы объявления

--- js/form-advert.js
+++ js/form-advert.js
@@ -1,7 +1,7 @@
-const formAdvertValidation = document.querySelector('.ad-form');
+const formAdvert = document.querySelector('.ad-form');
 
 const pristine = new Pristine(
-  formAdvertValidation,
+  formAdvert,
   {
     classTo: 'ad-form__element',
     errorTextParent: 'ad-form__element',
@@ -9,19 +9,20 @@ const pristine = new Pristine(
   }
 );
 
-const elementTitleAdvert = formAdvertValidation.querySelector('#title');
-const validateTitleAdvert = (value) => value.length < 30 && value.length > 100;
-const getErrorTextTitle = () => 'От 30 до 100 символов';
+const titleAdvert = formAdvert.querySelector('#title');
+const validateTitleAdvert = (value) => value.length >= 30 && value.length <= 100;
+const getErrorTitle = () => 'Длина заголовка должна быть от 30 до 100 символов';
 
 pristine.addValidator(
-  elementTitleAdvert,
+  titleAdvert,
   validateTitleAdvert,
-  getErrorTextTitle,
+  getErrorTitle,
 );
 
 // выбор поля валидации
-const elementTypeRoom = formAdvertValidation.querySelector('#type');
-const elementPriceRoom = formAdvertValidation.querySelector('#price');
+
+const typeRoom = formAdvert.querySelector('#type');
+const priceRoom = formAdvert.querySelector('#price');
 const minPriceHousing = {
   bungalow: 0,
   flat: 1000,
@@ -29,39 +30,126 @@ const minPriceHousing = {
   house: 5000,
   palace: 10000,
 };
+
 // условие валидации поля price
-const validatePriceAdvert = () => elementPriceRoom.value > minPriceHousing[elementTypeRoom.value];
-elementTypeRoom.addEventListener('change', () => {
-  elementPriceRoom.placeholder = minPriceHousing[elementTypeRoom.value];
-  elementPriceRoom.min = minPriceHousing[elementTypeRoom.value];
+
+const MAX_PRICE_HOUSING = 100000;
+const validatePriceAdvert = () => priceRoom.value >= minPriceHousing[typeRoom.value] && priceRoom.value <= MAX_PRICE_HOUSING;
+typeRoom.addEventListener('change', () => {
+  priceRoom.placeholder = minPriceHousing[typeRoom.value];
+  priceRoom.min = minPriceHousing[typeRoom.value];
 });
-const getTextErrorPrice = () => `Минимальная цена ${minPriceHousing[elementTypeRoom.value]}`;
+
+const getErrorPrice = () => {
+  if (priceRoom.value <= minPriceHousing[typeRoom.value]) {
+    return `Минимальная цена ${minPriceHousing[typeRoom.value]}`;
+  }else if (priceRoom.value >= MAX_PRICE_HOUSING) {
+    return `Максимальная цена ${MAX_PRICE_HOUSING}`;
+  }
+};
+
 pristine.addValidator(
-  elementPriceRoom,
+  priceRoom,
   validatePriceAdvert,
-  getTextErrorPrice,
+  getErrorPrice,
 );
 
 
-// просто хрень честно говоря, попытка написать хоть что то
-// const validateTimeIn = formAdvertValidation.querySelector('#timein');
-// const valueTimeIn = validateTimeIn.querySelectorAll('option');
-// console.log(validateTimeIn[1]);
-// const validateTimeOut = formAdvertValidation.querySelector('#timeout');
-// console.log(valueTimeIn);
-// const valueTimeOut = validateTimeOut.querySelectorAll('option');
-// console.log(validateTimeIn[1]);
+const timeIn = formAdvert.querySelector('#timein');
+const timeOut = formAdvert.querySelector('#timeout');
 
-// validateTimeIn.addEventListener('change', () => {
-//   for (let i = 0; i < validateTimeIn.length; i++) {
-//     if(valueTimeIn[i] === valueTimeOut[i]) {
-//       valueTimeOut.setAttribute('selected', true);
-//       return;
-//     }
-//   }
-// });
+// при выборе значения в одном поле, значение в другом поле должно поменяться
+timeIn.addEventListener('change', () => {
+  if (timeIn.value === '12:00') {
+    timeOut[1].removeAttribute('selected', 'selected');
+    timeOut[2].removeAttribute('selected', 'selected');
+    timeOut[0].setAttribute('selected', 'selected');
+    return;
+  }
+  if (timeIn.value === '13:00') {
+    timeOut[0].removeAttribute('selected', 'selected');
+    timeOut[2].removeAttribute('selected', 'selected');
+    timeOut[1].setAttribute('selected', 'selected');
+    return;
+  }
+  if (timeIn.value === '14:00') {
+    timeOut[0].removeAttribute('selected', 'selected');
+    timeOut[1].removeAttribute('selected', 'selected');
+    timeOut[2].setAttribute('selected', 'selected');
+  }
+});
 
-formAdvertValidation.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
+timeOut.addEventListener('change', () => {
+  if (timeOut.value === '12:00') {
+    timeIn[1].removeAttribute('selected', 'selected');
+    timeIn[2].removeAttribute('selected', 'selected');
+    timeIn[0].setAttribute('selected', 'selected');
+    return;
+  }
+  if (timeOut.value === '13:00') {
+    timeIn[0].removeAttribute('selected', 'selected');
+    timeIn[2].removeAttribute('selected', 'selected');
+    timeIn[1].setAttribute('selected', 'selected');
+    return;
+  }
+  if (timeOut.value === '14:00') {
+    timeIn[0].removeAttribute('selected', 'selected');
+    timeIn[1].removeAttribute('selected', 'selected');
+    timeIn[2].setAttribute('selected', 'selected');
+  }
+});
+
+const validateTimeInputs = () => timeIn.value === timeOut.value;
+const getErrorTextTimeIn = () => 'Время заезда должно соотвествовать времени выезда';
+const getErrorTextTimeOut = () => 'Время выезда должно соотвествовать времени заезда';
+
+pristine.addValidator(
+  timeIn,
+  validateTimeInputs,
+  getErrorTextTimeIn,
+);
+
+pristine.addValidator(
+  timeOut,
+  validateTimeInputs,
+  getErrorTextTimeOut,
+);
+
+const roomsInput = formAdvert.querySelector('#room_number');
+const capacityInput = formAdvert.querySelector('#capacity');
+
+const roomsToOption = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
+};
+
+const validateRoomsInput = () => roomsToOption[roomsInput.value].includes(capacityInput.value);
+
+const getRoomsError = () => {
+  if (roomsInput.value === '1') {
+    return 'Размещение для одного гостя';
+  }
+  if (roomsInput.value === '2') {
+    return 'Размещение от одного до двух гостей';
+  }
+  if (roomsInput.value === '3') {
+    return 'Размещение от одного до трех гостей';
+  }
+  if (roomsInput.value === '100') {
+    return 'Не для гостей';
+  }
+};
+
+pristine.addValidator(
+  capacityInput,
+  validateRoomsInput,
+  getRoomsError,
+);
+
+formAdvert.addEventListener('submit', (evt) => {
+  if(!pristine.validate()){
+    evt.preventDefault();
+  }
 });
