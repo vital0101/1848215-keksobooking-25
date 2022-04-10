71e97368082a094bd4042e485d1113f304a0b91b
Vitaliy Vaskin <vital0101@yandex.ru>
2022-04-10 10:15:40 +0300

    правки-3

--- js/page-states.js
+++ js/page-states.js
@@ -1,19 +1,13 @@
-// находим форму
 const advertForm = document.querySelector('.ad-form');
-// находим элементы формы
 const advertFormtInteractiveElements = advertForm.querySelectorAll('fieldset, button');
-// находим фильтр
 const mapFilterForm = document.querySelector('.map__filters');
-//находим элеметны фильра
 const mapFilterInteractiveElements = mapFilterForm.querySelectorAll('fieldset');
 
-//дезактивация формы
 const deactivateAdvertForm = () => {
   advertForm.classList.add('ad-form--disabled');
   advertFormtInteractiveElements.forEach((element) => element.setAttribute('disabled', 'disabled'));
 };
 
-//дезактивация фильтра
 const deactivateMapFilterForm = () => {
   mapFilterForm.classList.add('map__filters--disabled');
   mapFilterInteractiveElements.forEach((element) => element.setAttribute('disabled', 'disabled'));
@@ -24,13 +18,11 @@ export const deactivateForms = () => {
   deactivateMapFilterForm();
 };
 
-//активация формы
 export const activateAdvertForm = () => {
   advertForm.classList.remove('ad-form--disabled');
   advertFormtInteractiveElements.forEach((element) => element.removeAttribute('disabled', 'disabled'));
 };
 
-//активация фильтра
 export const activateMapFilterForm = () => {
   mapFilterForm.classList.remove('map__filters--disabled');
   mapFilterInteractiveElements.forEach((element) => element.removeAttribute('disabled', 'disabled'));
